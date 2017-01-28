03f97453f795dd8a09732f571b6bc848adc5c3e6
errorlife <1018915066@qq.com>
2017-01-29 02:36:57 +0800

    A hello demo based on the springboot and the maven

---
+++ src/main/java/cn/steffan/yunpan/hello/SampleController.java
@@ -0,0 +1,24 @@
+package cn.steffan.yunpan.hello;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+@EnableAutoConfiguration
+public class SampleController {
+
+	@RequestMapping("/")
+	@ResponseBody
+	String home(){
+		return "Hello World!";
+	}
+	
+	public static void main(String[] args) throws Exception {
+		// TODO Auto-generated method stub
+		SpringApplication.run(SampleController.class, args);
+	}
+
+}
