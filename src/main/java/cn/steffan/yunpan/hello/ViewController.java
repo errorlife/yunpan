539a0bdd3896a294ef9831e6db9953341a2add95
errorlife <1018915066@qq.com>
2017-02-01 23:43:32 +0800

    Daily Submission

--- src/main/java/cn/steffan/yunpan/hello/ViewController.java
+++ src/main/java/cn/steffan/yunpan/hello/ViewController.java
@@ -1,18 +1,22 @@
 package cn.steffan.yunpan.hello;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-@RequestMapping("ViewController")
+@RequestMapping("viewController")
 public class ViewController {
 	@RequestMapping("/index")
 	public String getIndex(){
-		return "index";
+		return "index.jsp";
 	}
 	
 	@RequestMapping("/")
-	public String get(){
-		return "index";
-	}
+    public String index(ModelMap map) {
+        // 加入一个属性，用来在模板中读取
+        map.addAttribute("host", "http://blog.didispace.com");
+        // return模板文件的名称，对应src/main/resources/templates/index.html
+        return "index.jsp";  
+    }
 }
